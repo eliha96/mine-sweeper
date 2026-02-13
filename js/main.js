0119452cf7f973158436447db4e2233fb958988b
Eli Haevry <eli.haevry@gmaail.com>
2026-02-13 22:02:36 +0200

    create first functions

--- js/main.js
+++ js/main.js
@@ -1,15 +1,40 @@
 'use strict'
-
-var gBoard
-
 var gLevel = {
     SIZE: 4,
     MINES: 2
 }
 
-var gGame = { 
-    isOn: false, 
+var gBoard = []
+
+var gGame = {
+    isOn: false,
     revealedCount: 0,
-    markedCount: 0, 
-    secsPassed: 0 
+    markedCount: 0,
+    secsPassed: 0
+}
+
+buildBoard()
+
+function createMat(rows, cols = rows) {
+    const mat = []
+    for (var i = 0; i < rows; i++) {
+        const row = []
+        for (var j = 0; j < cols; j++) {
+            row.push({
+                minesAroundCount: 0,
+                isRevealed: false,
+                isMine: false,
+                isMarked: false
+            })
+        }
+        mat.push(row)
+    }
+    return mat
+}
+
+function buildBoard() {
+    gBoard = createMat(gLevel.SIZE)
+    // place mines
+    gBoard[0][1].isMine = true
+    gBoard[1][0].isMine = true
 }
